dd406053afa5e14e858c86328396eab7e3ff6098
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2022-09-03 18:24:11 +0000

    move django secret keys to config file

--- age_estimation/settings.py
+++ age_estimation/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from configparser import RawConfigParser
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+config = RawConfigParser()
+config.read("config.cfg")
+skey = config["keys"]["django_secret"]
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-+^v(dpf2$-3$^)(ql&vpaa*_*%-ikao3*=f=%@wtqy_otw+h-p"
+SECRET_KEY = f"{skey}"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
